72d399677e589397034276259b9786424898aca0
Ross Kukulinski <ross.kukulinski@asti-usa.com>
2014-08-05 17:22:04 -0700

    Added mustache template generation

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,34 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 var rimraf = require('rimraf');
 var jeditor = require("gulp-json-editor");
+var mustache = require("gulp-mustache");
 var webserver = require('gulp-webserver');
 
-gulp.task('zip_learnyounode', ['clean'], function() {
+var wifi_ssid = 'this_ssid';
+var wifi_pw = 'password';
+
+
+
+gulp.watch('./templates/*.mustache', ['process_templates']);
+gulp.watch('./instructions.txt', ['move_instructions']);
+gulp.watch(['./*.css', './*.png'], ['move_static']);
+
+
+gulp.task('zip_learnyounode', function() {
   return gulp.src('./node_modules/learnyounode/**/*')
-    .pipe(zip('learnyounode.zip'))
-    .pipe(gulp.dest('./dist'));
+      .pipe(zip('learnyounode.zip'))
+      .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('zip_streamadventure', ['clean'], function() {
+gulp.task('zip_streamadventure', function() {
   return gulp.src('./node_modules/stream-adventure/**/*')
-    .pipe(zip('streamadventure.zip'))
-    .pipe(gulp.dest('./dist'));
+      .pipe(zip('streamadventure.zip'))
+      .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('move_instructions', ['clean'], function() {
+gulp.task('move_instructions', function() {
   return gulp.src(['instructions.txt'])
-    .pipe(gulp.dest('./dist/'));
+      .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('clean', function(cb) {
@@ -44,28 +55,48 @@ gulp.task('get_ip', function() {
   }
 });
 
-gulp.task('update_ip', ['clean', 'get_ip'], function() {
-  gulp.src('./config.json')
-    .pipe(jeditor({
-      'local_ip': localIP
-    }))
-    .pipe(gulp.dest('./dist'));
+gulp.task('update_ip', ['get_ip'], function() {
+  return gulp.src('./config.json')
+      .pipe(jeditor({
+        'local_ip': localIP
+      }))
+      .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('process_templates', function() {
+  return gulp.src("./templates/*.mustache")
+      .pipe(mustache({
+          wifi_ssid: wifi_ssid,
+          wifi_pw: wifi_pw,
+          ip: localIP
+      },
+      {
+        extension: '.html'
+      }))
+      .pipe(gulp.dest("./dist"));
+});
+
+gulp.task('move_static', function() {
+  return gulp.src(['./*.css', './*.png'])
+      .pipe(gulp.dest("./dist"));
 });
 
 gulp.task('run_webserver', [
   'zip_learnyounode',
   'zip_streamadventure',
   'move_instructions',
+  'process_templates',
+  'move_static',
   'update_ip'], function() {
-  gulp.src('./dist')
+  return gulp.src('./dist')
     .pipe(webserver({
       host: '0.0.0.0',
-      port: 8080
+      port: 80
     }))
 });
 
+
 gulp.task('default', [
-  'clean',
   'zip_learnyounode',
   'zip_streamadventure',
   'move_instructions',
